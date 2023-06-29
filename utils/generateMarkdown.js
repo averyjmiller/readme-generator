d0e94e4654aed0867472e0d4bf63592ad0ea503c
Avery Miller <averymiller2060@gmail.com>
2023-06-29 13:07:02 -0400

    added functionality to the renderLicenseBadge function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,11 +1,22 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 // ![Static Badge](https://img.shields.io/badge/License-MIT.svg)
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license === 'No license') return;
+
+  license.split(' ').join('_');
+  const badge = `![Static Badge](https://img.shields.io/badge/License-${license}-blue.svg)`;
+
+  return badge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license === 'No license') return;
+
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
